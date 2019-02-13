e5def90ddcc3b506f851ec04e1ee145d5acce489
AdrienRoussel <31723540+Lakuman@users.noreply.github.com>
2019-02-13 23:48:05 +0100

    Modify FlashLightTexture and add Crouch Command and Run Command
    - Add Texture on flashLight
    - Add Crouch Command LeftShift
    - Add Run Command RightShift
    - Replace mouse click movement by arrows

--- Escape Pyramide/Assets/Scripts/PlayerController.cs
+++ Escape Pyramide/Assets/Scripts/PlayerController.cs
@@ -5,31 +5,88 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float speed;
     public float jump;
+    public float crouchSpeed = 500f;
     public float rotationForce = 2f;
-	
-	// Update is called once per frame
-	void Update () {
-        Turn();
+    private bool isRunning = false;
+    private bool isCrouching = false;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        //RightShift
+        Run();
+        //ARROWS
         Move();
-	}
+        //ZQSD
+        Turn();
+        //LeftShift
+        Crouch();
+
+    }
     private void Move()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
         }
-        else if (Input.GetMouseButton(1))
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector3.back * speed * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
         }
     }
     private void Turn()
     {
         Vector3 v3 = new Vector3(0.0f, Input.GetAxis("Horizontal"), 0.0f);
-        transform.Rotate(v3 * rotationForce * Time.deltaTime);
-        if (Input.GetKey(KeyCode.Space))
+        transform.Rotate(v3 * rotationForce * Time.deltaTime); 
+    }
+    private void Run()
+    {
+        if (Input.GetKey(KeyCode.RightShift))
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            if (isRunning == false)
+            {
+                speed = speed + speed;
+                isRunning = true;
+            }
+        }
+        else 
+        {
+            if (isRunning == true)
+            {
+                speed = speed - (speed/2);
+                isRunning = false;
+
+            }
+        }
+
+    }
+    private void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            
+            if (isCrouching == false)
+            {
+                speed = speed - (speed / 2);
+                isCrouching = true;
+                transform.localScale = transform.localScale / 2;
+            }
+        }
+        else
+        {
+            if (isCrouching == true)
+            {
+                speed = speed * 2;
+                isCrouching = false;
+                transform.localScale = transform.localScale * 2;
+            }
         }
     }
 }
